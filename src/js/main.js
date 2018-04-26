27617659d630bff5abf574c540c94f1ae43e9732
Nataly <ncgarzon88@gmail.com>
2018-04-26 15:45:20 -0400

    Halfway through class updates

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,29 @@
 
+// can be called a factory function
+//I want to make it all self contained I will then call the function on the global scope while 
 
 let slideshow = function(time, selector) {
 
+	let currentSlideNumber = 0;
 	// find the element we're going to build the slideshow inside
 	let $slideshowContainer = document.querySelector(selector);
+
 	if (!$slideshowContainer) {
 		console.warn("Couldn't create slideshow, element not found: " + selector)
 		return false
 	}
+
+
+	setInterval(function() {
+		//remove .active from whatever slide currently has it
+
+		//instead of doing the document you can be more specific by replacing the document with the more specific item
+		let $active = $slideshowContainer.querySelector('.active');
+		//you can als do it like this but it would be more work for the browser
+		//let $active = document.querySelector(selector +' .active')
+		if ($active)$active.classList.remove('active');
+
+	}, time)
 	
 
 	return {
@@ -16,4 +32,58 @@ let slideshow = function(time, selector) {
 
 }
 
-let mySlideshow1 = new slideshow(3000, ".slideshow1")
+let mySlideshow1 = slideshow(3000, ".slideshow1")
+
+
+//so what we're doing is creating something like .play or .innerhtml it has something already built into the code. We're making things like .play outselves so we can give that to whatever coder is using this after and they can use it. They don't care how it works. They just care that they can use it. It allows developers to trust developers at the next level deeper.
+
+//The trust is how the sites are built. THATS OOP!!!! Object oriented programing!
+
+//so the things that we're making can be used as an object. 
+
+//SO I think I'm still confused on how the function is becoming an object. I see that we're returning the function as an object item now and it just becomes an object that can be called
+//############################################
+
+
+//setInterval for every 3 seconds 
+//remove .active from whichever one has it
+//add .active to the current slide that we will define in the variable. current slide num
+//we'll put them in an array
+
+//so make current slide number go up by one ++
+//and then active to slide one.
+//put a little extra bit to say that it won't go past 3. If current slide Num is too high( more than 3) reset back to 0, current slide num
+//then after reseting add.active to slide
+
+
+//if you push the letters through to the end and then also set each to a button that is assigned to the slide and then have a button to start doing the movement
+
+
+
+
+
+
+
+//If you do everything on the global scope then there are just a ton of variables hanging out
+//we're going to start to put the items inside the scope that we are. 
+//scoping out queries for elements. so we can call the element more specifically
+//you don't want such generic names in the outside scope and 
+
+
+//OOP l
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
